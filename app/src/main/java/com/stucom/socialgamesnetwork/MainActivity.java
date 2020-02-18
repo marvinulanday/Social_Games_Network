e296a0a6f481d9e076cf9f496519206be7451a70
Marvin Ulanday <marvinulanday@gmail.com>
2020-02-18 15:08:07 +0100

    LoginActivity creado

--- app/src/main/java/com/stucom/socialgamesnetwork/MainActivity.java
+++ app/src/main/java/com/stucom/socialgamesnetwork/MainActivity.java
@@ -2,7 +2,10 @@ package com.stucom.socialgamesnetwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.stucom.socialgamesnetwork.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
     }
 }
