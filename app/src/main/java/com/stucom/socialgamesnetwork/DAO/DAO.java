61c42a11a94054b6cf7e9064782159dc870aee6e
Artur Viader <djvatio@hotmail.com>
2020-03-14 12:37:03 +0100

    Arreglo conexion webservice

--- app/src/main/java/com/stucom/socialgamesnetwork/DAO/DAO.java
+++ app/src/main/java/com/stucom/socialgamesnetwork/DAO/DAO.java
@@ -1,6 +1,9 @@
 package com.stucom.socialgamesnetwork.DAO;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.android.volley.Request;
@@ -10,9 +13,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.stucom.socialgamesnetwork.model.Data;
 import com.stucom.socialgamesnetwork.model.User;
 import com.stucom.socialgamesnetwork.ui.login.MyCallback;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DAO {
     private User user;
@@ -26,7 +34,7 @@ public class DAO {
      * @param password Password escrita por el usuario
      */
     public void getUser(Context context, final MyCallback callback, final String username, final String password) {
-        RequestQueue queue = Volley.newRequestQueue(context);
+       /* RequestQueue queue = Volley.newRequestQueue(context);
         String url = "http://www.arturviader.com/socialgamesnetwork/login?username=" + username + "&password=" + password;
         Log.d("SGN", url);
         StringRequest request = new StringRequest(
@@ -51,7 +59,65 @@ public class DAO {
                     }
                 }
         );
+        queue.add(request);*/
+
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String URL = "https://www.arturviader.com/socialgamesnetwork/login";
+        StringRequest request = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override public void onResponse(String response) {
+
+                        Gson gson = new Gson();
+                        Type typeToken = new TypeToken<Data>() {}.getType();
+                        Data apiResponse = gson.fromJson(response.toString(), typeToken);
+
+                        if(apiResponse.getErrorCode()==0)
+                        {
+                            String token = apiResponse.getData().toString();
+
+                            guardatoken(token,username);
+                            user = new User(username, password);
+                            callback.login(user);
+                        }
+                        else
+                        {
+                            AlertDialog show = new AlertDialog.Builder(null)
+                                    .setTitle("Error")
+                                    .setMessage("Error: " + apiResponse.getErrorMsg())
+                                    .setNeutralButton("OK", null)
+                                    .show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override public void onErrorResponse(VolleyError error) {
+                AlertDialog show = new AlertDialog.Builder(null)
+                        .setTitle("Error")
+                        .setMessage("Network error")
+                        .setNeutralButton("OK", null)
+                        .show();
+            }
+        }) {
+            @Override protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
         queue.add(request);
     }
 
+
+
+
+
+    private void guardatoken(String token, String correu) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(null);
+        SharedPreferences.Editor ed = prefs.edit();
+        ed.putString("usertoken", token);
+        ed.putString("email", correu);
+        ed.commit();
+    }
 }
+
