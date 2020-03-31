298cefd19a26b3c8d51730e977590229f57492ae
Jonathan Cacay <jcacaynes@gmail.com>
2020-03-31 18:15:41 +0200

    Drawer menu options

---
+++ app/src/main/java/com/stucom/socialgamesnetwork/ProfileFragment.java
@@ -0,0 +1,6 @@
+package com.stucom.socialgamesnetwork;
+
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+}
---
+++ app/src/main/java/com/stucom/socialgamesnetwork/RankingFragment.java
@@ -0,0 +1,6 @@
+package com.stucom.socialgamesnetwork.ui.login;
+
+import androidx.fragment.app.Fragment;
+
+public class RankingFragment extends Fragment {
+}
