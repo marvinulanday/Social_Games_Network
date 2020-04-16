f97bb013f9d751c0aba86ab55e0f2f3bb67faf1f
Marvin Ulanday <marvinulanday@gmail.com>
2020-04-16 23:32:58 +0200

    Cambios de nombre de los archivos DAO

--- app/src/main/java/com/stucom/socialgamesnetwork/data/LoginDataSource.java
+++ app/src/main/java/com/stucom/socialgamesnetwork/data/LoginDataSource.java
@@ -2,7 +2,7 @@ package com.stucom.socialgamesnetwork.data;
 
 import android.content.Context;
 
-import com.stucom.socialgamesnetwork.DAO.DAO;
+import com.stucom.socialgamesnetwork.DAO.SgnDAO;
 import com.stucom.socialgamesnetwork.exceptions.SGNException;
 import com.stucom.socialgamesnetwork.model.User;
 import com.stucom.socialgamesnetwork.ui.login.MyCallback;
@@ -13,8 +13,8 @@ import com.stucom.socialgamesnetwork.ui.login.MyCallback;
 public class LoginDataSource {
 
     public void login(Context context, MyCallback callback, String username, String password) {
-        DAO dao = new DAO();
-        dao.getUser(context, callback, username, password);
+        SgnDAO sgnDAO = new SgnDAO();
+        sgnDAO.getUser(context, callback, username, password);
     }
 
     public Result<User> loginResult(User user) {
