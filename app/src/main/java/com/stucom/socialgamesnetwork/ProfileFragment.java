b5aebdef73e84e35965728ba401e025983b972fc
xGauss05 <jcacaynes@gmail.com>
2020-04-17 17:21:22 +0200

    Acabado diseño registro. Falta implementar botón perfil.

--- app/src/main/java/com/stucom/socialgamesnetwork/ProfileFragment.java
+++ app/src/main/java/com/stucom/socialgamesnetwork/ProfileFragment.java
@@ -4,15 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProfileFragment extends Fragment {
+    private ImageView ivAvatar;
+    private FloatingActionButton btnEdit;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, null);
+        ivAvatar = view.findViewById(R.id.ivAvatarProfile);
+        ivAvatar.setImageResource(R.drawable.user);
+        btnEdit = view.findViewById(R.id.btnEdit);
+
+        return view;
     }
+
+
+
+
 }
